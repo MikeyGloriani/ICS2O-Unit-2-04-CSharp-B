e09b1bdb10207ff9d840888a99df42ea75b6e117
MikeyGloriani <113134835+MikeyGloriani@users.noreply.github.com>
2022-10-17 14:32:55 -0400

    code change

--- main.cs
+++ main.cs
@@ -1,16 +1,31 @@
 // Created by: Mikey Gloriani
 // Created on: Oct 2022
 //
-// This program displays, "Hello, World!"
+// This program displays, the area of a triangle
 
 
 using System;
 
-class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+class Program {
+    public static void Main (string[] args) {
+        // This function accepts user input
+        int Base;
+        int height;
+        int area;
+
+        Console.WriteLine("This program calculates the area and perimeter of a rectangle.");
+        Console.WriteLine("");
+
+        Console.Write("Enter the base (cm): ");
+        Base = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the height (cm): ");
+        height = Convert.ToInt32(Console.ReadLine());
+
+        area = (Base * height)/2;
+
+        Console.WriteLine("");
+        Console.WriteLine("The area is: " + area + " cm². ");
+
         Console.WriteLine("\nDone.");
     }
 }
